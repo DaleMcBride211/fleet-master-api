c8ccfa396fb9ebeebf5afe2a357b8d8ecd18d7f0
ealbanca <ealbanca@byui.edu>
2026-03-11 20:54:13 -0600

    Created getsingleasset method

--- controllers/assetscontroller.js
+++ controllers/assetscontroller.js
@@ -29,7 +29,21 @@ const getAllAssets = async (req, res, next) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+const getSingleAsset = async (req, res) => {
+    // #swagger.tags = ['Assets']
+    // #swagger.description = 'Get a single asset by its MongoDB ID.'
+    /* #swagger.parameters['id'] = { description: 'Asset ID' } */
+    try {
+        const asset = await Users.findById(req.params.id);
+        if (!asset) return res.status(404).json({ message: "Record not found" });
+        res.status(200).json(asset);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
     
 module.exports = {
-    getAllAssets
+    getAllAssets,
+    getSingleAsset
 };
