b97376e8e0a454697526cd62bd375b79f800ef42
ealbanca <ealbanca@byui.edu>
2026-03-12 19:20:18 -0600

    removed swagger route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-router.get('/', require('./swagger'));
 router.use('/users', require('./users'));
 router.use('/assets', require('./assets'));
 // router.use('/maintenancehistory', require('./maintenancehistory'));
