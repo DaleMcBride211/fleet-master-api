b97df7f00093bf0e62b5eb39374bd777ae724050
ealbanca <ealbanca@byui.edu>
2026-03-14 13:43:09 -0600

    Added support for CORS

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ app.use(express.json());
 app.use('/', require('./routes'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
+// Add support for CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}. Docs at http://localhost:${PORT}/api-docs/`);
 });
