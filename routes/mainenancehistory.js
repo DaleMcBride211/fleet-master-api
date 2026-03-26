d9bded146e6010dfce0e249a3b691f4ef81f1fb7
ealbanca <ealbanca@byui.edu>
2026-03-25 19:50:11 -0600

    Created maintenace route

---
+++ routes/mainenancehistory.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const maintenanceHistoryController = require('../controllers/maintenancehistorycontroller')
+
+// Routes for maintenance history
+
+router.get('/', maintenanceHistoryController.getAllMaintenanceHistory);
+    
+router.get('/:id', maintenanceHistoryController.getSingleMaintenanceHistory);
+
+router.post('/', maintenanceHistoryController.createMaintenanceHistory);
+
+router.put('/:id', maintenanceHistoryController.updateMaintenanceHistory);
+
+router.delete('/:id', maintenanceHistoryController.deleteMaintenanceHistory);
+
+module.exports = router;
